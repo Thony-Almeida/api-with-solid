8e0fe640b303ef627571af83cdd1c3a940ec5123
Anthony Almeida <aladasilva@rd.com.br>
2024-11-20 16:16:25 -0300

    feat: add code style configs

--- build/env/index.js
+++ build/env/index.js
@@ -1,10 +1,12 @@
 "use strict";
-var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
     for (let key of __getOwnPropNames(from))
@@ -13,20 +15,14 @@ var __copyProps = (to, from, except, desc) => {
   }
   return to;
 };
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-  // If the importer is in node compatibility mode or this is not an ESM
-  // file that has been converted to a CommonJS file using a Babel-
-  // compatible transform (i.e. "__esModule" has not been set), then set
-  // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-  mod
-));
-
-// src/app.ts
-var import_fastify = __toESM(require("fastify"));
-var app = (0, import_fastify.default)();
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // src/env/index.ts
+var env_exports = {};
+__export(env_exports, {
+  env: () => env
+});
+module.exports = __toCommonJS(env_exports);
 var import_config = require("dotenv/config");
 var import_zod = require("zod");
 var envSchema = import_zod.z.object({
@@ -39,11 +35,7 @@ if (_env.success === false) {
   throw new Error("Invalid environment variables!");
 }
 var env = _env.data;
-
-// src/server.ts
-app.listen({
-  host: "0.0.0.0",
-  port: env.PORT
-}).then(() => {
-  console.log("\u{1F680} Server is running");
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  env
 });
